8c46d4ee2e126b8451db3666fa42a1945b856492
pavo <josephelias1234@gmail.com>
2026-03-15 22:20:47 -0500

    Specify region for SNS client initialization in monitor_quality.py

--- scripts/monitor_quality.py
+++ scripts/monitor_quality.py
@@ -16,7 +16,7 @@ from datetime import datetime, timedelta
 from statistics import mean
 
 s3 = boto3.client("s3",region_name="us-east-1")
-sns = boto3.client("sns")
+sns = boto3.client("sns",region_name="us-east-1")
 secrets = boto3.client("secretsmanager")
 session = boto3.Session(region_name="us-east-1")
 
